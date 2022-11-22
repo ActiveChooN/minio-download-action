84778a89b90841b49696da06de10d2d4fc88cfdf
Dmitry Kalinin <8168727+ActiveChooN@users.noreply.github.com>
2022-11-22 18:31:59 +0100

    Added missed api arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 set -euo pipefail
 
-sh -c "mc alias set action $ENDPOINT $ACCESS_KEY $SECRET_KEY $API"
+sh -c "mc alias set action $ENDPOINT $ACCESS_KEY $SECRET_KEY --api $API"
 
 FULL_REMOTE_PATH=action
 
